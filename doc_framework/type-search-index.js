14d4891f9201bd1447c6c64b7cf96cbe7d6319f5
Gerrit von Laue <gerritvonlaue@mbp-von-gerrit.fritz.box>
2023-03-30 20:07:59 +0200

    Kommentarfunktion

---
+++ doc_framework/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"liste","l":"Command"},{"p":"liste","l":"CommandList"},{"p":"liste","l":"Element"}];updateSearchResults();
